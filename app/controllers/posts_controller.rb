b2f2928eaad936d50965244a153f2a25fbf3dd6e
David Bradway <david.bradway@gmail.com>
2014-02-11 22:29:54 +0100

    chages from tutorial parts 7-9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,20 +23,28 @@ class PostsController < ApplicationController
   end
   
   def edit
-  
+    @post = Post.find(params[:id])  
   end
   
   def update
-  
+    @post = Post.find(params[:id])
+
+    #if @post.update_attributes(params[:post])
+    if @post.update_attributes(post_params)
+      redirect_to posts_path, :notice => "Your post has been updated!"
+    else
+      render "edit"
+    end
   end
   
   def destroy
-
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, :notice => "Your post has been deleted"
   end
   
   def post_params
     params.require(:post).permit(:title, :content)
   end
-  
   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   # This line below was intended to plug a security vulnerability, but I got errors:
   #RuntimeError: `attr_accessible` is extracted out of Rails into a gem. Please use new recommended protection model for params(strong_parameters) or add `protected_attributes` to your Gemfile to use old one.
   #attr_accessible :title, :content
+
+  validates :title, :content, :presence => true
+  validates :title, :length => { :minimum => 2 }
+  validates :title, :uniqueness => { :message => "title taken" }
 end
